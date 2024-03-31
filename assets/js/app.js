2422d6cf988ef368e5ce4970d1e5facdf9ec63d3
Judith <judith.sanmartin.s@gmail.com>
2024-03-30 23:56:54 -0300

    probando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
+class Consultorio {
+    constructor(Paciente) {
+        this.Pacientes = [];
+
+    }
+    get getPaciente() {
+        return this.Pacientes;
+    }
+
+    set setPaciente(Paciente) {
+        this.Pacientes.push(Paciente);
+    }
+}
+//Metodo
+Consultorio.prototype.buscarPorNombre = function(nombre) {
+    return this.Pacientes.find(paciente => paciente.nombre === nombre);
+}
+
+Consultorio.prototype.mostrardatos = function () {
+    this.Pacientes.forEach(paciente => {
+        console.log("Nombre:", paciente.nombre);
+        console.log("Edad:", paciente.edad);
+        console.log("RUT:", paciente.rut);
+        console.log("Diagnóstico:", paciente.diagnostico);
+    })
+}
+
+
 class Paciente {
     constructor(nombre, edad, rut, diagnostico) {
         this.nombre = nombre;
@@ -5,42 +33,19 @@ class Paciente {
         this.rut = rut;
         this.diagnostico = diagnostico;
     }
-    get getNombre() {
-        return this.nombre
-    }
-    get getEdad() {
-        return this.edad
-    }
-    get getRut() {
-        return this.rut
-    }
-    get getDiagnostico() {
-        return this.ediagnostico
-    }
-
-    set setNombre(value) {
-        this.nombre = value
-    }
-    set setEdad(value) {
-        this.edad = value
-    }
-    set setRut(value) {
-        this.rut = value
-    }
-    set setDiagnostico(value) {
-        this.diagnostico = value
-    }
-
-
-
-
-    saludar = function () {
-        return `${this.nombre} dice hola`
-    }
+    
 }
-const personaUno = new Paciente("Berta", 76, "6.383.128-K", "Diabetes")
-const personaDos = new Paciente("Leoncio", 70, "7.007.335-8", "Asmatico")
-const personaTres = new Paciente("Judith", 37, "16.692.403-0", "obesidad")
+//instanciar
+const consultorio = new Consultorio();
 
-console.log(personaTres.getnombre)
-console.log(personaTres.saludar())
+
+const pacienteUno = new Paciente("Berta", 76, "6.383.128-K", "Diabetes")
+const pacienteDos = new Paciente("Leoncio", 70, "7.007.335-8", "Asmatico")
+const pacienteTres = new Paciente("Judith", 37, "16.692.403-0", "obesidad")
+
+Consultorio.setPaciente = pacienteUno;
+Consultorio.setPaciente = pacienteDos;
+Consultorio.setPaciente = pacienteTres;
+
+console.log(consultorio.buscarPorNombre("Berta"))
+consultorio.mostrarTodosDatos()
