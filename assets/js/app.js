828eb098a0e790683c90772acde5a7d55a71ffc7
Judith <judith.sanmartin.s@gmail.com>
2024-03-31 00:18:35 -0300

    corrigiendo mayusculas

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,29 +3,28 @@ class Consultorio {
         this.Pacientes = [];
 
     }
-    get getPaciente() {
+    get getPacientes() {
         return this.Pacientes;
     }
 
-    set setPaciente(Paciente) {
+    set setPacientes(Paciente) {
         this.Pacientes.push(Paciente);
     }
-}
-//Metodo
-Consultorio.prototype.buscarPorNombre = function(nombre) {
-    return this.Pacientes.find(paciente => paciente.nombre === nombre);
-}
 
-Consultorio.prototype.mostrardatos = function () {
-    this.Pacientes.forEach(paciente => {
-        console.log("Nombre:", paciente.nombre);
-        console.log("Edad:", paciente.edad);
-        console.log("RUT:", paciente.rut);
-        console.log("Diagnóstico:", paciente.diagnostico);
-    })
+    //Metodo
+    buscarPorNombre = function (nombre) {
+        return this.Pacientes.find(paciente => paciente.nombre === nombre);
+    }
+
+    mostrardatos = function () {
+        this.Pacientes.forEach(paciente => {
+            console.log("Nombre:", paciente.nombre);
+            console.log("Edad:", paciente.edad);
+            console.log("RUT:", paciente.rut);
+            console.log("Diagnóstico:", paciente.diagnostico);
+        })
+    }
 }
-
-
 class Paciente {
     constructor(nombre, edad, rut, diagnostico) {
         this.nombre = nombre;
@@ -33,19 +32,50 @@ class Paciente {
         this.rut = rut;
         this.diagnostico = diagnostico;
     }
-    
+
+    get getNombre() {
+        return this.nombre;
+    }
+    get getEdad() {
+        return this.edad;
+    }
+    get getRut() {
+        return this.rut;
+    }
+    get getDiagnostico() {
+        return this.diagnostico;
+    }
+
+    set setNombre(value) {
+        this.nombre = value;
+    }
+    set setEdad(value) {
+        this.edad = value;
+    }
+    set setRut(value) {
+        this.rut = value;
+    }
+    set setDiagnostico(value) {
+        this.diagnostico = value;
+    }
+
 }
+
 //instanciar
 const consultorio = new Consultorio();
 
 
-const pacienteUno = new Paciente("Berta", 76, "6.383.128-K", "Diabetes")
-const pacienteDos = new Paciente("Leoncio", 70, "7.007.335-8", "Asmatico")
-const pacienteTres = new Paciente("Judith", 37, "16.692.403-0", "obesidad")
+const pacienteUno = new Paciente("Berta", 76, "6.383.128-K", "Diabetes");
+const pacienteDos = new Paciente("Leoncio", 70, "7.007.335-8", "Asmatico");
+const pacienteTres = new Paciente("Judith", 37, "16.692.403-0", "obesidad");
 
-Consultorio.setPaciente = pacienteUno;
-Consultorio.setPaciente = pacienteDos;
-Consultorio.setPaciente = pacienteTres;
+consultorio.setPacientes = pacienteUno;
+consultorio.setPacientes = pacienteDos;
+consultorio.setPacientes = pacienteTres;
 
-console.log(consultorio.buscarPorNombre("Berta"))
-consultorio.mostrarTodosDatos()
+console.log(Consultorio.getPacientes)
+console.log("Datos de Judith:");
+console.log(consultorio.buscarPorNombre("Judith"));
+
+console.log("Todos los pacientes:");
+consultorio.mostrardatos();
